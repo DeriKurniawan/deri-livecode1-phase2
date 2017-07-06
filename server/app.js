83c204eb4ab053aaad3198b111c71db7cb8e990a
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-07-06 10:27:22 +0700

    membuat app.js dan membuat food model

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//require mongoose and connect
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost:27017/derikurniawan1';
+mongoose.connect(url, (err, res)=>{
+  if(err){
+    console.log('something wrong on database');
+  }
+  console.log('you are connoct to database on ', url);
+});
+
+//var foods = require('./routes/foods');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//app.use('/api/food', foods);
+
+app.listen(3000, ()=>{
+  console.log('you are on port 3000');
+})
+
+module.exports = app;
