d8cbfd52210e5de921e78438f7a7329da0c7ed50
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-07-06 11:25:07 +0700

    membuat update dan delete restaurant model

--- server/controllers/restaurant.js
+++ server/controllers/restaurant.js
@@ -46,4 +46,53 @@ model.create = function(req, res){
   })
 }
 
+model.update = function(req, res){
+    Restaurant.findById(req.params.id, (err, data)=>{
+      if(err){
+        res.status(500).send({
+          message: 'something wrong in databse',
+          error: err
+        })
+      } else {
+        //console.log('ini req.body di food.update : ', req.body);
+        //console.log('ini data di food.update : ', data);
+        let body = req.body;
+        data.name = body.name || data.name;
+        data.owner = body.owner || data.owner;
+        data.address = body.address || data.address;
+
+        data.save((err, result)=>{
+          if(err){
+            res.status(400).send({
+              message: 'something wrong in databse',
+              error: err
+            })
+          } else {
+            res.send({
+              msg: 'Update data restaurant is success',
+              data: result
+            })
+          }
+        })
+      }
+    })
+}
+
+model.delete = function(req, res){
+  let id = req.params.id;
+  Restaurant.findByIdAndRemove(req.params.id, (err, result)=>{
+    if(err){
+      res.status(400).send({
+        message: 'something wrong about database',
+        error: err
+      })
+    } else {
+      res.send({
+        msg: 'delete restaurant is success!!',
+        data: result
+      })
+    }
+  })
+}
+
 module.exports = model;
--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -4,5 +4,7 @@ var router = require('express').Router();
 router.get('/', Restaurant.showAll);
 router.get('/:id', Restaurant.getOne);
 router.post('/', Restaurant.create);
+router.put('/:id', Restaurant.update);
+router.delete('/:id', Restaurant.delete);
 
 module.exports = router;
